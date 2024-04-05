9a33ec723705766fce60fac158b75be7a2f9bece
maykonmarcosjunior <maykon.marcos.junior@grad.ufsc.br>
2024-04-05 12:13:13 -0300

    Inclusão do SCN na viewport

--- T1_1/src/ViewPort.py
+++ T1_1/src/ViewPort.py
@@ -9,11 +9,25 @@ class ViewPort(tk.Canvas):
         self.configure(scrollregion=self.bbox("all"))
 
         self.__width = width_
-        self.__height = height_
-        
+        self.__height = height_        
 
-    def viewport_transform(self, x: float, y: float) -> list[float]:
-        vx = ((x + 1) * (self.__width) / 2)
-        vy = (1 - (y + 1) / 2) * (self.__height)
+
+    def viewport_transform(self, x: float, y: float, xw_min: float,
+                           xw_max: float, yw_min: float, yw_max: float,
+                           SCN: bool = True) -> list[float]:
+        if SCN:
+            x *= self.__width
+            y *= self.__height
+        vx = (
+            (x - xw_min)
+            * (self.__width)
+            / (xw_max - xw_min)
+        )
+        vy = (1 - (y - yw_min) / (yw_max - yw_min)) * (self.__height)
+        
+        # vx = (x + 1) / (2) * self.__width + 10
+        # vy = (1 - (y + 1) / (2)) * self.__height + 10
+
+        # print("\nvx:", vx, "vy:", vy, "\n")
 
         return [vx, vy]
