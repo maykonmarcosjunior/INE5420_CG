d17bafb46fbe71b230e4e11f5f9d3885986b1ba7
maykonmarcosjunior <maykon.marcos.junior@grad.ufsc.br>
2024-04-05 12:12:41 -0300

    Exercício Clipping

--- T1_1/teste.py
+++ T1_1/teste.py
@@ -1,42 +1,94 @@
 from src.Objetos.Objeto2D import Objeto2D
+from src.Window import Window
 
 '''
 obj1
 (100, 100),(500, 100),(500, 500)
+obj2
+(90, 90),(510, 90),(510, 510),(90, 510)
 '''
+def clip_NLN(line, window):
+    def compute_outcode(x, y, window):
+        xmin, xmax, ymin, ymax = window
+        code = 0
+        if x < xmin:
+            code |= 1
+        elif x > xmax:
+            code |= 2
+        if y < ymin:
+            code |= 4
+        elif y > ymax:
+            code |= 8
+        return code
 
-obj1 = Objeto2D('obj1', [(0, 1), (1, 2), (2, 3)], 'Wireframe')
+    def clip_line(p1, p2, window):
+        xmin, xmax, ymin, ymax = window
+        outcode1 = compute_outcode(*p1, window)
+        outcode2 = compute_outcode(*p2, window)
+        accept = False
 
-print('original:', obj1)
-print('centro geométrico:', obj1.geometric_center())
+        while True:
+            if not (outcode1 | outcode2):
+                accept = True
+                break
+            elif outcode1 & outcode2:
+                break
+            else:
+                x = None
+                y = None
+                outcode_out = outcode1 if outcode1 else outcode2
 
-print('\nteste 1')
-obj1.translation(1, 1)
-print('transladado:', obj1)
-obj1.translation(-1, -1)
-print('desfeito:', obj1)
+                if outcode_out & 1:
+                    x = xmin
+                    y = p1[1] + (p2[1] - p1[1]) * (xmin - p1[0]) / (p2[0] - p1[0])
+                elif outcode_out & 2:
+                    x = xmax
+                    y = p1[1] + (p2[1] - p1[1]) * (xmax - p1[0]) / (p2[0] - p1[0])
+                elif outcode_out & 4:
+                    y = ymin
+                    x = p1[0] + (p2[0] - p1[0]) * (ymin - p1[1]) / (p2[1] - p1[1])
+                elif outcode_out & 8:
+                    y = ymax
+                    x = p1[0] + (p2[0] - p1[0]) * (ymax - p1[1]) / (p2[1] - p1[1])
 
-print('\nteste 2')
-obj1.scaling(2, 2)
-print('aumentado:', obj1)
-obj1.scaling(0.5, 0.5)
-print('desfeito:', obj1)
+                if outcode_out == outcode1:
+                    p1 = (x, y)
+                    outcode1 = compute_outcode(x, y, window)
+                else:
+                    p2 = (x, y)
+                    outcode2 = compute_outcode(x, y, window)
 
-print('\nteste 3')
-obj1.rotation("RotationType.world_center", 30)
-print('rotação simples:', obj1)
-obj1.rotation("RotationType.world_center", -30)
-print('desfeito:', obj1)
+        if accept:
+            return [p1, p2]
+        else:
+            return []
+
+    clipped_line = clip_line(line[0], line[1], window)
+    return clipped_line
 
 
-print('\nteste 4')
-obj1.rotation("RotationType.any_point", 30, 1.0, 1.0)
-print('rotação em torno de (1, 1):', obj1)
-obj1.rotation("RotationType.any_point", -30, 1.0, 1.0)
-print('desfeito:', obj1)
+window = Window(None, 1, 1)
+A = [(-4, 0.5), (-0.1, 2.5)]
+B = [(-1.5, 0.0), (0.9, 0.4)]
+C = [(-1.5, -1.0), (1.0, 0.5)]
+D = [(0.4, -0.2), (2.8, 1.5)]
 
-print('\nteste 5')
-obj1.rotation("RotationType.object_center", 30)
-print('rotação em torno do centro geométrico:', obj1)
-obj1.rotation("RotationType.object_center", -30)
-print('desfeito:', obj1)
+print("A:", A)
+print(window.cohen_sutherland(A))
+print(window.liang_barsky(A))
+print(clip_NLN(A, (-1, 1, -1, 1)))
+
+print("\nB:", B)
+print(window.cohen_sutherland(B))
+print(window.liang_barsky(B))
+print(clip_NLN(B, (-1, 1, -1, 1)))
+
+print("\nC:", C)
+print(window.cohen_sutherland(C))
+print(window.liang_barsky(C))
+print(clip_NLN(C, (-1, 1, -1, 1)))
+
+print("\nD:", D)
+print(window.cohen_sutherland(D))
+print(window.liang_barsky(D))
+print(clip_NLN(D, (-1, 1, -1, 1)))
