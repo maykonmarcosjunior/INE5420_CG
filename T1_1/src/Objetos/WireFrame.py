a0b75a28670bf17513505140cac5af918df23f8a
anaard <anareginadelazzari@gmail.com>
2024-04-07 08:52:48 -0300

    fill getter wireframe

--- T1_1/src/Objetos/WireFrame.py
+++ T1_1/src/Objetos/WireFrame.py
@@ -1,5 +1,10 @@
 from src.Objetos.Objeto2D import Objeto2D
 
 class WireFrame(Objeto2D):
-    def __init__(self, name, coordenadas=[(0, 0), (1, 1), (0, 2)], obj_type="Wireframe", color="000000"):
+    def __init__(self, name, coordenadas=[(0, 0), (1, 1), (0, 2)], obj_type="Wireframe", color="000000", fill=False):
         super().__init__(name, coordenadas, obj_type, color)
+        self.__fill = fill
+
+    @property
+    def fill(self) -> bool:
+        return self.__fill
